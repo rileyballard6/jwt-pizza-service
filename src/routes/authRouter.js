ca1f197319f769a6e9fdeb02cc87c293010ae09d
Riley Ballard <rileyrb01@gmail.com>
2025-03-18 10:56:27 -0600

    remove metrics code

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const config = require('../config.js');
 const { asyncHandler } = require('../endpointHelper.js');
 const { DB, Role } = require('../database/database.js');
-const metrics = require("../metrics.js");
 
 const authRouter = express.Router();
 
@@ -59,10 +58,7 @@ async function setAuthUser(req, res, next) {
 // Authenticate token
 authRouter.authenticateToken = (req, res, next) => {
   if (!req.user) {
-    metrics.authenticationRequests(false);
     return res.status(401).send({ message: 'unauthorized' });
-  } else {
-    metrics.authenticationRequests(true);
   }
   next();
 };
--- src/service.js
+++ src/service.js
@@ -4,12 +4,10 @@ const orderRouter = require('./routes/orderRouter.js');
 const franchiseRouter = require('./routes/franchiseRouter.js');
 const version = require('./version.json');
 const config = require('./config.js');
-const metrics = require("./metrics.js")
 
 const app = express();
 app.use(express.json());
 app.use(setAuthUser);
-app.use(metrics.getRequests);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
